f05089529bfdd7b5c1559b0288aec298238981c6
wdqyj-sx <wenyeqv@163.com>
2020-07-24 14:54:34 +0800

    修改

--- index.js
+++ index.js
@@ -4,4 +4,37 @@ map = L.map('map', {
     maxZoom: 18,
     zoom: 16
 });
+let flag = true;
 L.supermap.tiledMapLayer(url).addTo(map);
+
+function Fullwidth() {
+    console.log('hah')
+    map.setZoom(14, {})
+
+}
+//放大
+function enlarge() {
+    map.zoomIn()
+
+}
+//缩小
+function narrow() {
+    map.zoomOut()
+}
+//平移
+function translation() {
+    if (flag) {
+        map.dragging.disable()
+
+    } else {
+        map.dragging.enable()
+    }
+    flag = !flag
+}
+let url1 = 'http://localhost:8090/iserver/services/data-sxx/rest/data'
+L.supermap.featureService(url1)
+    .getFeaturesBySQL({
+        datasetNames: 'Hospital'
+    }, function(result) {
+        console.log(result)
+    })
