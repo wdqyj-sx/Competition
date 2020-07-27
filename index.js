03074cbba60285f71d8cd233de7d89c161e6280b
wdqyj-sx <wenyeqv@163.com>
2020-07-27 19:44:02 +0800

    分支

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let map;
-// let url = "http://localhost:8090/iserver/services/map-sxx/rest/maps/City@EmergDS";
-let url = "http://localhost:8090/iserver/services/map-sx/rest/maps/City%40EmergDS";
+let url = "http://localhost:8090/iserver/services/map-sxx/rest/maps/City@EmergDS";
+// let url = "http://localhost:8090/iserver/services/map-sx/rest/maps/City%40EmergDS";
 map = L.map('map', {
     center: [30.61, -1500016.3],
     maxZoom: 18,
@@ -35,11 +35,17 @@ function translation() {
     flag = !flag
 }
 query();
-
+//附近医院查询
 function query() {
     let param = new SuperMap.QueryBySQLParameters({
-        queryParams: {
-            name: ''
-        }
+        queryParams: [{
+            name: "Hospital@EmergDS",
+            attributeFilter: "1 = 1"
+        }]
     })
+    L.supermap
+        .queryService(url)
+        .queryBySQL(param, function(serviceResult) {
+            console.log(serviceResult)
+        })
 }
